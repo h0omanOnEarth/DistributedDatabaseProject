b658bc19152d38f140f27dcfa44aa32aedeb34e7
Clarissa Gracienne <clarissagracia.cg@gmail.com>
2023-12-13 22:26:27 +0700

    Update cart.blade.php

--- resources/views/screens/customer/cart.blade.php
+++ resources/views/screens/customer/cart.blade.php
@@ -9,19 +9,31 @@
                     @foreach ($cartItems as $cartItem)
                         <div class="col">
                             <div class="card">
-                                <div class="card-body">
-                                    @php
-                                        $product = \App\Models\Product::find($cartItem->products_id);
-                                    @endphp
-                                    <h5 class="card-title">{{ $product->nama }}</h5>
-                                    <p class="card-text">Price: Rp{{ $product->harga }}</p>
-                                    <p class="card-text">Qty: <span id="qty_{{ $cartItem->id }}">{{ $cartItem->qty }}</span>
-                                    </p>
-                                    <div class="btn-group" role="group" aria-label="Quantity">
-                                        <button type="button" class="btn btn-secondary"
-                                            onclick="updateQty({{ $cartItem->id }}, 'decrement', {{ $product->id }})">-</button>
-                                        <button type="button" class="btn btn-secondary"
-                                            onclick="updateQty({{ $cartItem->id }}, 'increment', {{ $product->id }})">+</button>
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                        <img src="https://source.unsplash.com/400x400/?product" class="card-img-top"
+                                            style="object-fit: cover;" alt="Product Image">
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            @php
+                                                $product = \App\Models\Product::find($cartItem->products_id);
+                                            @endphp
+                                            <h5 class="card-title text-right" style="font-weight: bold">{{ $product->nama }}
+                                            </h5>
+                                            <p class="card-text text-right">Price:
+                                                Rp{{ $product->harga }}</p>
+                                            <p class="card-text text-right">Qty: <span
+                                                    id="qty_{{ $cartItem->id }}">{{ $cartItem->qty }}</span></p>
+                                            <div class="btn-group" role="group" aria-label="Quantity">
+                                                <button type="button" class="btn btn-secondary"
+                                                    onclick="updateQty({{ $cartItem->id }}, 'decrement', {{ $product->id }})">-</button>
+                                                <button type="button" class="btn btn-secondary"
+                                                    onclick="updateQty({{ $cartItem->id }}, 'increment', {{ $product->id }})">+</button>
+                                            </div>
+                                            <!-- Tambahkan sesuatu agar tidak terlalu monoton -->
+                                            <p class="card-text mt-2">{{ $product->description }}</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -32,12 +44,25 @@
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Subtotal</h5>
-                        <p class="card-text" id="subtotal">Rp{{ $subtotal }}</p>
+                        <h5 class="card-title" style="font-weight: bold;">Subtotal</h5>
+                        <p class="card-text" id="subtotal" style="font-weight: bold">Rp{{ $subtotal }}</p>
+                        <!-- Tambahkan keterangan delivery di bawah subtotal -->
+                        <p class="card-text">Delivery: Rp0,00</p>
+
+                        <!-- Tambahkan penjelasan tentang delivery fee yang gratis -->
+                        <p class="card-text">
+                            Enjoy FREE delivery for orders above Rp10,000! Take advantage of this special offer and get
+                            your favorite items delivered to your doorstep without any additional cost.
+                        </p>
+
+                        <!-- Tambahkan paragraf terms and conditions -->
+                        <p class="card-text mt-2">By proceeding with your purchase, you agree to our <a
+                                href="{{ url('/terms-and-conditions') }}" target="_blank">Terms and Conditions</a>.</p>
                         <button class="btn btn-primary">Checkout</button>
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 
