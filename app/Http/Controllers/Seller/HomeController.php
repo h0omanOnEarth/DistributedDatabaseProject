5dfcf7764416955256ae97798b4b734751a39271
Angelizines <47268689+angelivine@users.noreply.github.com>
2023-12-15 09:02:55 +0700

    try
    try

--- app/Http/Controllers/Seller/HomeController.php
+++ app/Http/Controllers/Seller/HomeController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Seller;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     //
     public function homePage(){
         $user = Auth::user();
+        $products = DB::connection('oracle_c')->select("SELECT * FROM products");
+
+        dd($products);
         return view('screens.seller.home',["user"=>$user]);
     }
 }
