0aa77602d161a5686fad711f88dcf0fd25fd22bd
aaronlinggo <aaronlinggosatria@gmail.com>
2023-12-21 12:28:59 +0700

    Add Cronjob sidebar

--- resources/views/layouts/main_admin.blade.php
+++ resources/views/layouts/main_admin.blade.php
@@ -125,8 +125,12 @@
             href="{{ url('/admin/products') }}">Products</a>
         <a class="{{ request()->is('admin/transactions*') ? 'active' : '' }}"
             href="{{ url('/admin/transactions') }}">Transactions</a>
-        <a class="{{ request()->is('admin/cronjob_manual*') ? 'active' : '' }}"
+        <a class="{{ request()->is('admin/transactions*') ? 'active' : '' }}"
             href="{{ url('/admin/logs') }}">AUTOMATIC LOGS</a>
+        <a class="{{ request()->is('admin/transactions*') ? 'active' : '' }}"
+            href="{{ url('/admin/cronjob_manual') }}">RUN CRONJOB MANUAL</a>
+        <a class="{{ request()->is('admin/transactions*') ? 'active' : '' }}" href="{{ url('/admin/logs') }}">Automatic
+            Logs</a>
         <a class="{{ request()->is('logout') ? 'active' : '' }}" href="{{ url('/logout') }}">Logout</a>
     </div>
 
